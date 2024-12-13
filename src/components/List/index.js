c82603de285ea687e2652517c6e90a560bf3eac6
RibahDev <joao.ribeiro17@outlook.com>
2024-12-13 13:04:27 -0300

    Avançando com o efeito

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -13,7 +13,7 @@ export default function List({ language }) {
                 text={code}
                 language={language}
                 showLineNumbers={true}
-                wrapLongLines
+                wrapLongLines={true}
                 highlight="1,2,3,4,5,6,7,8,9,10"
                 theme={dracula}
             />
